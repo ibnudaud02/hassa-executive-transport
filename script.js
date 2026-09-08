6ff6259dc6e9ec5cefe31a0bf091be8da0a136d1
ibnudaud02 <ibnudaud02@gmail.com>
2026-09-08 15:52:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Scroll Animation
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(
+".features, .services, .fleet, .booking-process, .reviews, .faq, .stats, .cta, .contact"
+).forEach(el => observer.observe(el));
+
+// Header Resize
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 80){
+        header.style.padding = "12px 8%";
+    }else{
+        header.style.padding = "20px 8%";
+    }
+
+});
